38e36054287dfbf5b4fa8390d3ddeb6d0e343a71
Matthew Wingerden <matthew.wingerden@gmail.com>
2023-01-15 16:26:22 -0800

    Added all necessary requirements and finished Lab01.

--- src/Lab01.java
+++ src/Lab01.java
@@ -2,47 +2,81 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Lab01 extends JFrame{
-    private JFrame window;
-    private JPanel cyan;
-    private JPanel pink;
-    private JPanel yellow;
-    private JPanel orange;
+
+    public JMenuBar createMenuBar() {
+        //Create and Add MenuBar
+        JMenuBar jMenuBar = new JMenuBar();
+
+        //Create Menu
+        JMenu file = new JMenu("File");
+        JMenu about = new JMenu("About");
+
+        //Create and Add Items to Appropriate Menus
+        JMenuItem open = new JMenuItem("Open");
+        JMenuItem save = new JMenuItem("Save");
+        JMenuItem quit = new JMenuItem("Quit");
+        file.add(open);
+        file.add(save);
+        file.add(quit);
+
+        //Add Menus to MenuBar
+        jMenuBar.add(file);
+        jMenuBar.add(about);
+
+        //Return MenuBar
+        return jMenuBar;
+    }
+
+    public JPanel createColoredBoxes() {
+        //Create JPanel to Hold Colors
+        JPanel boxes = new JPanel(new GridLayout(2, 2));
+
+        //instantiate
+        JPanel cyan = new JPanel(new GridLayout(1, 1));
+        JPanel pink = new JPanel(new GridLayout(1, 1));
+        JPanel yellow = new JPanel(new GridLayout(1, 1));
+        JPanel orange = new JPanel(new GridLayout(1, 1));
+
+        //Cyan
+        cyan.add(new JLabel("Hola"));
+        cyan.setBackground(Color.CYAN);
+        //Pink
+        pink.add(new JLabel("World!"));
+        pink.setBackground(Color.PINK);
+        //Yellow
+        yellow.add(new JLabel("Hello"));
+        yellow.setBackground(Color.YELLOW);
+        //Orange
+        orange.add(new JLabel("Mundo!"));
+        orange.setBackground(Color.ORANGE);
+
+        //Add to Boxes
+        boxes.add(yellow);
+        boxes.add(pink);
+        boxes.add(cyan);
+        boxes.add(orange);
+
+        //Return Colored Boxes
+        return boxes;
+    }
+
+    public void createAndShowGUI() {
+        //Create and set up window.
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content.
+        setJMenuBar(createMenuBar());
+        add(createColoredBoxes());
+
+        //Establish misc.
+        setTitle("Lab 01");
+        setSize(450, 350);
+        setVisible(true);
+    }
+
 
     public static void main(String [] args) {
         Lab01 lab01 = new Lab01();
-        lab01.window = new JFrame();
-        lab01.setLayout(new GridLayout(2, 2));
-
-        //instantiate
-        lab01.cyan = new JPanel();
-        lab01.pink = new JPanel();
-        lab01.yellow = new JPanel();
-        lab01.orange = new JPanel();
-
-        //Cyan
-        JLabel hello = new JLabel("Hola");
-        lab01.cyan.add(hello);
-        lab01.cyan.setBackground(Color.CYAN);
-        //Pink
-        lab01.pink.add(new JLabel("World!", SwingConstants.LEFT));
-        lab01.pink.setBackground(Color.PINK);
-        //Yellow
-        lab01.yellow.add(new JLabel("Hello", SwingConstants.CENTER));
-        lab01.yellow.setBackground(Color.YELLOW);
-        //Orange
-        lab01.orange.add(new JLabel("Mundo!", SwingConstants.CENTER));
-        lab01.orange.setBackground(Color.ORANGE);
-
-        //Add
-        lab01.add(lab01.yellow);
-        lab01.add(lab01.pink);
-        lab01.add(lab01.cyan);
-        lab01.add(lab01.orange);
-
-        lab01.setTitle("Lab 01");
-        lab01.setSize(400, 400);
-        lab01.setLocationRelativeTo(null);
-        lab01.setVisible(true);
-        lab01.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        lab01.createAndShowGUI();
     }
 }
