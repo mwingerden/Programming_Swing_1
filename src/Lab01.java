6b5c82fe6c1b1c2624b372c5331110ab4a5cdf86
Matthew Wingerden <matthew.wingerden@gmail.com>
2023-01-16 11:37:32 -0800

    Added JavaDoc comments.

--- src/Lab01.java
+++ src/Lab01.java
@@ -1,8 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * <p>Lab01 is a class that creates a GUI from Java Swing. This GUI holds a
+ * JMenuBar and a JPanel formatted as a 2x2 grid to hold different
+ * colored sub panels. Once instantiated, then the user would have to call
+ * createAndShowGUI() to create and view the GUI. The class contains a main
+ * that demonstrates the instantiation and call of the function; creating
+ * the GUI. Extends JFrame to call the functions to set up the GUI.<p>
+ *
+ * @author      Matthew Wingerden
+ * @version     1.0
+ * @since       2023-16-01
+ */
+
 public class Lab01 extends JFrame{
 
+    /**
+     * <p>Creates and returns a menu bar for the window. The menu bar just holds
+     * a File and About menu with File containing just three menu items: Open
+     * Save, and Quit. None of the options in the menu bar work when clicked.<p>
+     *
+     * @return      a JMenuBar with the current menu and menu items as described
+     *              above
+     */
     public JMenuBar createMenuBar() {
         //Create and Add MenuBar
         JMenuBar jMenuBar = new JMenuBar();
@@ -27,6 +48,13 @@ public class Lab01 extends JFrame{
         return jMenuBar;
     }
 
+    /**
+     * <p>Creates and returns a Panel for the window. The panel is formatted in a
+     * 2x2 grid and contains colored sub panels(in order: cyan, pink, yellow, and orange) that
+     * contains a word.<p>
+     *
+     * @return      a JPanel as described above.
+     */
     public JPanel createColoredBoxes() {
         //Create JPanel to Hold Colors
         JPanel boxes = new JPanel(new GridLayout(2, 2));
@@ -60,6 +88,11 @@ public class Lab01 extends JFrame{
         return boxes;
     }
 
+    /**
+     * <p>Sets up the window and adds the menu bar and colored panels, creating
+     * a standard GUI.<p>
+     *
+     */
     public void createAndShowGUI() {
         //Create and set up window.
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -74,7 +107,10 @@ public class Lab01 extends JFrame{
         setVisible(true);
     }
 
-
+    /**
+     * <p>A main that demonstrates the Lab 01 GUI.<p>
+     *
+     */
     public static void main(String [] args) {
         Lab01 lab01 = new Lab01();
         lab01.createAndShowGUI();
